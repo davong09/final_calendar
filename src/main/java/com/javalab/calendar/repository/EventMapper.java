b84939555136fa251e4521f2c3473a91abd7def2
davong09 <davin0910@naver.com>
2024-08-27 01:55:50 +0900

    수진이꺼 합친거

--- src/main/java/com/javalab/calendar/repository/EventMapper.java
+++ src/main/java/com/javalab/calendar/repository/EventMapper.java
@@ -2,27 +2,16 @@ package com.javalab.calendar.repository;
 
 import com.javalab.calendar.vo.EventVo;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+/*
+ * @author sujin
+ */
 @Mapper
-@Repository
 public interface EventMapper {
-
-	// 특정 ID의 이벤트 조회
-	EventVo getEvent(@Param("eventId") int eventId);
-
-	// 모든 이벤트 조회
-	List<EventVo> listEvent();
-
-	// 새로운 이벤트 생성
-	int createEvent(EventVo eventVo);
-
-	// 이벤트 수정
-	int updateEvent(EventVo eventVo);
-
-	// 이벤트 삭제
-	int deleteEvent(@Param("eventId") int eventId);
+	void insertEvent(EventVo eventVo);
+	List<EventVo> selectAllEvents();
+	EventVo selectEventById(int id);
+	void updateEvent(EventVo eventVo);
+	void deleteEvent(int id);
 }
--- src/main/java/com/javalab/calendar/vo/EventVo.java
+++ src/main/java/com/javalab/calendar/vo/EventVo.java
@@ -11,12 +11,13 @@ import org.springframework.format.annotation.DateTimeFormat;
  * 일정 도메인 클래스
  * - EventVo: 데이터베이스의 EVENT 테이블과 매핑되는 객체
  * - 데이터베이스의 상태를 그대로 반영하며, 이를 통해 데이터베이스로부터 데이터를 읽거나 쓸 수 있음
+ * @author sujin
  */
-@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자 생성
-@NoArgsConstructor  // 파라미터 없는 기본 생성자 생성
-@Getter @Setter     // 각 필드에 대한 getter와 setter 메서드 생성
-@ToString           // toString 메서드 자동 생성
-@Builder            // 빌더 패턴 구현
+@AllArgsConstructor  // 모든 필드를 파라미터로 받는 생성자 생성
+@NoArgsConstructor   // 파라미터 없는 기본 생성자 생성
+@Getter @Setter      // 각 필드에 대한 getter와 setter 메서드 생성
+@ToString            // toString 메서드 자동 생성
+@Builder             // 빌더 패턴 구현
 public class EventVo implements Serializable {
     private static final long serialVersionUID = 1L;
 
